7a20d7516e360e8849cd8fcb02ca255f541edf62
arvicxyz <dudeamirfahd@gmail.com>
2016-08-30 10:11:41 +0800

    Modify and add comments on AzureConnection class

--- AzureBackend/AzureBackend.Portable/Models/AzureConnection.cs
+++ AzureBackend/AzureBackend.Portable/Models/AzureConnection.cs
@@ -13,9 +13,17 @@ namespace AzureBackend.Shared
 
         public AzureConnection()
         {
+            // 1. Create a new EasyMobileServiceClient.
             client = EasyMobileServiceClient.Create();
-            client.Initialize("http://azurebackendloginregister.azurewebsites.net/");
+
+            // 2. Initialize the library with the URL of the Azure Mobile App you created in Step #1.
+            // Example: http://appservicehelpers.azurewebsites.net
+            client.Initialize("Your_Mobile_App_Backend_Url_Here");
+
+            // 3. Register a model with the EasyMobileServiceClient to create a table.
             client.RegisterTable<User>();
+
+            // 4. Finalize the schema for our database. All table registrations must be done before this step.
             client.FinalizeSchema();
         }
     }
